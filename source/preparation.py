9171946598a0b9c9afa6b5d6f005976f7e1eba37
João Ferreira <noobcsf@hotmail.com>
2018-12-21 17:28:17 +0000

    Started working on the database, added read and write helpers

--- source/preparation.py
+++ source/preparation.py
@@ -4,9 +4,12 @@ import tkinter as tk
 import tkinter.filedialog
 from PIL import Image
 from shared import *
+import json
+from pprint import pprint
+
 
 class Application(tk.Frame):
-
+  
   def say_hi(self):
     print("Hello")
   
@@ -25,47 +28,100 @@ class Application(tk.Frame):
   def click(self, event):
     print(event.x,event.y)
     text = "{0}, {1}".format(event.x, event.y)
-    self.DEBUG.insert(INSERT, text)
+    self.DEBUG.config(text=text)
+  
+  def _on_image_opened(self, opened):
 
+    for btn in self.image_dependent_buttons:
+      btn.config(state=tk.NORMAL if opened else tk.HIDDEN)
 
   def open_image(self, path):
-    image = cv.imread(path, cv.IMREAD_COLOR)
-    tkImage = image_cv_2_tk(image)
-    self.IMAGE_PANEL.configure(image=tkImage)
-    self.IMAGE_PANEL.image = tkImage
-    self.IMAGE_PANEL.bind('<Button-1>', self.click)
+    cv.namedWindow("selected", cv.WINDOW_NORMAL)
+    self.selected_image = cv.imread(path, cv.IMREAD_COLOR)
+    self.database.store_image(path)
+    cv.imshow("selected", self.selected_image)
+    self._on_image_opened(True)
+  
+  def calculate_features(self):
+    kp, des = calculate_key_points_akaze(self.selected_image)
+    return (kp, des)
+  
+  def _show_keypoints(self):
+    kp, _ = self.calculate_features()
+    self.show_keypoints(kp)
+  
+  def show_keypoints(self, keypoints):
+    kp = keypoints
+    img2 = self.selected_image.copy()
+    cv.drawKeypoints(img2, kp,img2)
+    self.DEBUG.config(text='Test..')
+    cv.namedWindow('keypoints', cv.WINDOW_NORMAL)
+    cv.imshow('keypoints', img2)
+    self.DEBUG.config(text='Showing')
+  
+  def load_features(self):
+    kp, _ = load_features('test.b')
+    self.show_keypoints(kp)
+    pass
+  
+  def store_features(self):
+    kp, des = self.calculate_features()
+    store_features('test.b', kp, des)
+    pass
 
   def create_widgets(self):
+    self.image_dependent_buttons = []
 
-    self.DEBUG = tk.Label()
-    self.DEBUG.config(text='Test')
-    self.DEBUG.pack(side=tk.TOP)
-
-    self.IMAGE_PANEL = tk.Label()
-    self.IMAGE_PANEL.pack(side=tk.LEFT)
-
-    self.QUIT = tk.Button(self)
+    self.QUIT = tk.Button()
     self.QUIT['text'] = "Quit"
     self.QUIT['fg'] = "red"
     self.QUIT['command'] = self.quit
     self.QUIT.pack(side=tk.LEFT)
 
-    self.OPEN_IMG = tk.Button(self)
+    self.OPEN_IMG = tk.Button()
     self.OPEN_IMG['text'] = 'Open'
     self.OPEN_IMG['command'] = self.open_image_dialog
     self.OPEN_IMG.pack(side=tk.LEFT)
 
-    
+    self.SHOW_KP = tk.Button()
+    self.SHOW_KP['state'] = tk.DISABLED
+    self.SHOW_KP['text'] = 'Key Points'
+    self.SHOW_KP['command'] = self._show_keypoints
+    self.SHOW_KP.pack(side=tk.LEFT)
+
+    self.STORE_FEATURES = tk.Button()
+    self.STORE_FEATURES['state'] = tk.DISABLED
+    self.STORE_FEATURES['text'] = 'Store Features'
+    self.STORE_FEATURES['command'] = self.store_features
+    self.STORE_FEATURES.pack(side=tk.LEFT)
+
+    self.LOAD_FEATURES = tk.Button()
+    self.LOAD_FEATURES['state'] = tk.DISABLED
+    self.LOAD_FEATURES['text'] = 'Load Features'
+    self.LOAD_FEATURES['command'] = self.load_features
+    self.LOAD_FEATURES.pack(side=tk.LEFT)
+
+    self.image_dependent_buttons.append(self.SHOW_KP)
+    self.image_dependent_buttons.append(self.STORE_FEATURES)
+    self.image_dependent_buttons.append(self.LOAD_FEATURES)
+
+
+    self.DEBUG = tk.Label()
+    self.DEBUG.config(text='Test')
+    self.DEBUG.pack(side=tk.BOTTOM)
 
     
   def __init__(self, master=None):
     tk.Frame.__init__(self, master)
+    self.database = Database('db.json')
     self.pack()
     self.create_widgets()
 
 
 def main():
   root = tk.Tk()
+  root.title('Toolbox')
+  root.geometry('500x50')
   app = Application(master=root)
   app.mainloop()
   root.destroy()
--- source/shared.py
+++ source/shared.py
@@ -2,9 +2,107 @@ import cv2 as cv
 import numpy as np
 from PIL import Image
 from PIL import ImageTk
+from pprint import pprint
+import pickle
+import json
 
+class Database():
+  def __init__(self, path):
+    self.dict = {'images': {}}
+    self.path = path
+    try:
+      with open(path, 'r') as f:
+        if f == None:
+          return
+        self.dict = json.load(f)
+    except FileNotFoundError:
+      print('File Not Found Creating new one')
+      pass
+  
+  def save(self):
+    with open(self.path, 'w') as f:
+      json.dump(self.dict, f)
+
+  def retrieve_keys(self):
+    return self.dict['images'].keys()
+
+  def store_image(self, path):
+    if not self.dict.__contains__('images'):
+      self.dict['images']={}
+
+    images = self.dict['images']
+    if(not images.__contains__(path)):
+      images[path] = {}
+
+    self.save()
 
 def image_cv_2_tk(image):
   image = cv.cvtColor(image, cv.COLOR_BGR2RGB)
   pilFormat = Image.fromarray(image)
   return ImageTk.PhotoImage(pilFormat)
+
+def calculate_key_points(image, detector):
+  gray = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
+  kp, des = detector.detectAndCompute(image, None)
+  return (kp, des)
+
+def calculate_key_points_sift(image):
+  sift = cv.xfeatures2d.SIFT_create()
+  return calculate_key_points(image, sift)
+
+def calculate_key_points_surf(image):
+  surf = cv.xfeatures2d.SURF_create(400)
+  return calculate_key_points(image, surf)
+
+def calculate_key_points_akaze(image):
+  akaze = cv.AKAZE_create()
+  return calculate_key_points(image, akaze)
+
+def calculate_key_points_kaze(image):
+  kaze = cv.KAZE_create()
+  return calculate_key_points(image, kaze)
+
+def features_to_pickle(keypoints, descriptors):
+  array = []
+
+  for index, kp in enumerate(keypoints):
+    tmp = (
+      kp.pt, kp.size, 
+      kp.angle, kp.response, 
+      kp.octave, kp.class_id, 
+      descriptors[index])
+
+    array.append(tmp)
+  return array
+
+def pickle_to_features(data):
+  keypoints=[]
+  descriptors=[]
+  for point in data:
+    kp = cv.KeyPoint(
+      x=point[0][0], y=point[0][1],
+      _size=point[1], _angle=point[2],
+      _response=point[3], _octave=point[4],
+      _class_id=point[5]
+      )
+    descriptor = point[6]
+    keypoints.append(kp)
+    descriptors.append(descriptor)
+  return keypoints, np.array(descriptors)
+
+def store_features(path, keypoints, descriptors):
+  array = features_to_pickle(keypoints, descriptors)
+  pickle.dump(array, open(path, 'wb'))
+
+def load_features(path):
+  array = pickle.load(open(path, 'rb'))
+  return pickle_to_features(array)
+
+
+
+
+if __name__ == "__main__":
+    pass
+
+
+
