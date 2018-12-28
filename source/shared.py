307021d4ee847991bc1f3657679dc8d9165872ae
Nuno <nunojorge90@hotmail.com>
2018-12-28 17:32:42 +0000

    comments

--- source/shared.py
+++ source/shared.py
@@ -9,7 +9,9 @@ import os
 
 MIN_MATCH_COUNT = 10
 
-
+# Database that contains features, points of interest, and images
+# Features are stored in ./features
+# Points of interest and map data is stored in db.json
 class Database():
     def __init__(self, path):
         self.cached_features = {}
@@ -33,16 +35,17 @@ class Database():
         except FileNotFoundError:
             print('File Not Found Creating new one')
             pass
+    # Get calibration
     def get_calibration(self):
       return self.calibration
-
+    # Save data to json file
     def save(self):
         with open(self.path, 'w') as f:
             json.dump(self.dict, f)
-
+    # Retrieve list of images
     def retrieve_keys(self):
         return [k for k in self.dict['images'].keys()]
-
+    # Add new point of interest
     def add_poi(self, poi):
         obj = {}
         obj['name'] = poi
@@ -50,31 +53,31 @@ class Database():
         self.dict['poi'].append(obj)
         self.selected_poi = len(self.dict['poi'])-1
         self.save()
-
+    # Add image to existing point of interest
     def add_poi_image(self, path):
         self.dict['poi'][self.selected_poi]['image'] = path
         self.save()
-
+    # Check if the current selected image has any points of interest
     def has_pois(self):
         return len(self.dict['poi']) != 0
-
+    # Get current selected POI (for preparation program)
     def get_poi(self):
         if len(self.dict['poi']) == 0:
             return None
         return self.dict['poi'][self.selected_poi]['name']
-
+    # Get image of currently selected POI (for preparation program)
     def get_poi_image(self):
         if len(self.dict['poi']) == 0:
             return None
         return self.dict['poi'][self.selected_poi]['image']
 
-
+    # Get list of POIs
     def get_pois(self):
         return self.dict['poi']
-
+    # Get label of current POI
     def get_poi_label(self):
         return '%s %d/%d' % (self.get_poi(), self.selected_poi + 1, len(self.dict['poi']))
-
+    # Change currently selected POI to next one (for preparation)
     def next_poi(self):
         self.selected_poi = (self.selected_poi + 1) % len(self.dict['poi'])
 
@@ -85,24 +88,24 @@ class Database():
             obj['pois'] = {}
 
         return obj['pois']
-
+    # Retrieve list of pois on an image
     def retrieve_pois(self, path):
         return self._retrieve_pois(path)
-
+    # Associate a created POI to an image
     def associate_poi(self, path, coordinates, poiName=None, update=True):
         pois = self._retrieve_pois(path)
         pois[self.get_poi() if poiName is None else poiName] = coordinates
         if update:
             self.update_key_points(path)
         self.save()
-    
+    # Set scale of a certain image
     def set_scale(self, path, scale):
         images = self.retrieve_images_dict()
         obj = images[path]
         obj['scale'] = scale
 
         self.save()
-    
+    # Get scale of certain image
     def get_scale(self, path):
         images = self.retrieve_images_dict()
         obj = images[path]
@@ -112,7 +115,7 @@ class Database():
         except:
             pass
         return obj['scale'] if not value is None else 1
-
+    # Get the best homography between the features in 'image_features' and all images using SIFT
     def calculate_best_homogragy(self, image_features):
 
         for someImage in self.retrieve_keys():
@@ -124,6 +127,7 @@ class Database():
                 return (T, self.retrieve_pois(someImage), self.get_scale(someImage))
 
         return ((None, None, None), self.retrieve_pois(someImage), 0)
+    # Retrieve features of a previously computed image
     def retrieve_features(self, path):
         images = self.retrieve_images_dict()
         obj = images[path]
@@ -166,17 +170,17 @@ class Database():
                 new_point = homography_point(H, point)
                 new_point = (int(new_point[0]), int(new_point[1]))
                 self.associate_poi(key, new_point, name, False)
-
+    # Get list of images
     def retrieve_images_dict(self):
         if not self.dict.__contains__('images'):
             self.dict['images'] = {}
 
         return self.dict['images']
-
+    # Check if a map exists
     def file_exists(self, path):
         images = self.retrieve_images_dict()
         return (images.__contains__(path))
-
+    # Store a map
     def store_image(self, path):
         if len(path) == 0:
             return
@@ -204,33 +208,33 @@ def image_cv_2_tk(image):
     pilFormat = Image.fromarray(image)
     return ImageTk.PhotoImage(pilFormat)
 
-
+# Compute the key points and descriptors using a certain detector
 def calculate_key_points(image, detector):
     gray = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
     kp, des = detector.detectAndCompute(image, None)
     return (kp, des)
 
-
+# Use SIFT to detect keypoints
 def calculate_key_points_sift(image):
     sift = cv.xfeatures2d.SIFT_create()
     return calculate_key_points(image, sift)
 
-
+# Use SURF to detect keypoints
 def calculate_key_points_surf(image):
     surf = cv.xfeatures2d.SURF_create(400)
     return calculate_key_points(image, surf)
 
-
+# Use AKAZE to detect keypoints
 def calculate_key_points_akaze(image):
     akaze = cv.AKAZE_create()
     return calculate_key_points(image, akaze)
 
-
+# Use KAZE to detect keypoints
 def calculate_key_points_kaze(image):
     kaze = cv.KAZE_create()
     return calculate_key_points(image, kaze)
 
-
+# Store keypoints and descriptors to file by serializing objects
 def features_to_pickle(keypoints, descriptors):
     array = []
 
@@ -244,7 +248,7 @@ def features_to_pickle(keypoints, descriptors):
         array.append(tmp)
     return array
 
-
+# Store keypoints and descriptors to file by serializing objects
 def pickle_to_features(data):
     keypoints = []
     descriptors = []
@@ -260,19 +264,21 @@ def pickle_to_features(data):
         descriptors.append(descriptor)
     return keypoints, np.array(descriptors)
 
-
+# Store keypoints and descriptors to file by serializing objects
 def store_features(path, keypoints, descriptors):
     array = features_to_pickle(keypoints, descriptors)
     pickle.dump(array, open(path, 'wb'))
 
-
+# Load keypoints and descriptors from file by deserializing objects
 def load_features(path):
     array = pickle.load(open(path, 'rb'))
     return pickle_to_features(array)
 
+# Store camera calibration matrix and distortion coefficients
 def store_camera_calibration(path, matrix, dist_coefs):
   np.save(path, np.array([matrix, dist_coefs]))
 
+# Load camera calibration matrix and distortion coefficients
 def load_camera_calibration(path):
   return np.load(path)
 
@@ -281,6 +287,7 @@ def homography_point(H, p):
   dp = (rp[0] / rp[2], rp[1] / rp[2])
   return dp
 
+# Compute homography using feature points
 def compute_homography(features1, features2):
     des1 = features1[1]
     des2 = features2[1]
@@ -313,6 +320,8 @@ def distance_to_text(distance):
         return '{:.2f} km'.format(distance/1000)
 
     return '{:.2f} m'.format(distance)
+
+# Use camera calibration matrix and distortion matrix to get homography and rotation/translation vectors
 def compute_transformations_matrix(features1, features2, intrinsic_matrix, coef_points):
     des1 = features1[1]
     des2 = features2[1]
